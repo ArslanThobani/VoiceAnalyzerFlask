1e5d7d969de64fa1c5c3f1cfa1fc762e94b563a6
thobaniarslan <thobaniarslan@gmail.com>
2019-09-21 15:53:48 +0530

    Added port number in main.py

--- main.py
+++ main.py
@@ -46,4 +46,7 @@ def analyze(filename):
     print(test_exm[0,0], test_exm[0,1])
     return test_exm[0,0], test_exm[0,1]
 
-app.run(threaded=True , host='0.0.0.0')
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(threaded=True, host='0.0.0.0', port=port)
