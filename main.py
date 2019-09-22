2bbfc359dcf86c84a93e6a5ddc764cddcc146268
thobaniarslan <thobaniarslan@gmail.com>
2019-09-22 13:17:32 +0530

    Some changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ This is a temporary script file.
 """
 import os
 import flask
-from flask import request
+from flask import request,render_template
 from flask import jsonify, make_response
 import pickle
 import uuid
@@ -38,13 +38,38 @@ def uploadfile():
         f = request.file['file']
         f.save(f.filename)
         return "success"
+
+
+@app.errorhandler(500)
+def internal_error(error):
+
+    return "Internal Server Error",500
+
+@app.errorhandler(404)
+def not_found(error):
+    return "Not Found",404
+
     
 @app.route('/upload',methods=['POST'])
 def upload():
     f = request.files['file']
-    f.save(f.filename)
+    f.save("static//"+f.filename)
     conf, nonconf = analyze(f.filename)
-    return str(conf)
+    conf=5
+    happy=20
+    sad=22
+    neutral = 30
+    disgust=63
+    anger = 21
+    progressbar = {} 
+    progressbar["confidence"] = float(conf)
+    progressbar["happy"] = float(happy)
+    progressbar["sad"] = float(sad)
+    progressbar["neutral"] = float(neutral)
+    progressbar["disgust"] = float(disgust)
+    progressbar["anger"] = float(anger)
+    filePath = "static/"+f.filename
+    return render_template('analysis.html', title='Voxers - Analysis', progressbar=progressbar, filePath = filePath)
     
 
 def analyze(filename):
